373b55cec3edf3dabbaa6562d40a756cc4139375
John Stegall <jstegall@unexpecteddevelopment.org>
2012-08-09 21:29:46 -0700

    Fixed missing ASP.Net MVC App_Start files. Added CSS 3 spinners. Initial implementation of metaforms (metadata-built forms). Added initial implementation of adaptable (HTML table) jQuery plugin.

---
+++ Website/App_Start/BundleConfig.cs
@@ -0,0 +1,30 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Dragline
+{
+  public class BundleConfig
+  {
+    // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+    public static void RegisterBundles(BundleCollection bundles)
+    {
+      bundles.Add(new ScriptBundle("~/javascript/dragline")
+        .Include("~/scripts/jquery.form.js",
+          "~/scripts/jquery-ui-{version}.js",
+          "~/scripts/knockout-bindings.js",
+          "~/scripts/modal.js",
+          "~/scripts/responsive-navigation.js",
+          "~/scripts/toasty.js"));
+
+      bundles.Add(new StyleBundle("~/css/dragline")
+        .Include("~/css/normalize-{version}.css",
+          "~/css/elements.css",
+          "~/css/classes.css",
+          "~/css/printer-friendly.css",
+          "~/css/responsive.css",
+          "~/css/site.css",
+          "~/css/modal.css",
+          "~/css/toasty.css"));
+    }
+  }
+}
